e2fe2ec09c1572b75e9388e774284367f4be60b1
MaksymBaz <79503555+Bazylevych@users.noreply.github.com>
2023-06-02 00:39:13 +0300

    Create form

--- main.js
+++ main.js
@@ -12,4 +12,14 @@ app.get("/", (_, res) => {
   res.render("index");
 });
 
+app.post("/", (req, res) => {
+  const promt = req.body.promt;
+  const size = req.body.size;
+  const number = req.body.number;
+
+  console.log(promt, size, number);
+
+  res.render("index");
+});
+
 app.listen(3000, () => console.log("server started..."));
